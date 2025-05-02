73ec8f05fea7f16683e0c2f1f6d9f803e1efaae9
Patrick <mugishapatrick730@gmail.com>
2025-05-02 15:15:04 +0200

    feat:Add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
 import HomePage from './components/Homepage'; // Import HomePage
-import ShopPage from './components/ShopPage'; // Import ShopPage
+import ShopPage from './components/ShopPage'; // Import ShopPag
+import Navbar from './components/Navbar';
 
 
 function App() {
  return(
   <div>
+    <Navbar/>
     <h1>My shopping App</h1>
     <Routes>
         <Route path='/' element={<HomePage/>}></Route>
