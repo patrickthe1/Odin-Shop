36ff45cda05cba8cce9461ee68f9a1744f67d4df
Patrick <mugishapatrick730@gmail.com>
2025-05-02 14:21:22 +0200

    Initialize project with Placeholder components

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage(){
+    return (
+        <div>
+            <h1>Home page</h1>
+            <p>Welcome to our shop!</p>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/components/ShopPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ShopPage() {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+      <p>Browse our products here.</p>
+    </div>
+  );
+}
+
+export default ShopPage;
