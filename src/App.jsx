58d71da0208a074be32b4f3d203f1f2329b22f3c
Patrick <mugishapatrick730@gmail.com>
2025-05-02 14:32:36 +0200

    Configure basic routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; 
+import HomePage from './components/Homepage'; // Import HomePage
+import ShopPage from './components/ShopPage'; // Import ShopPage
+
+
 function App() {
+ return(
+  <div>
+    <h1>My shopping App</h1>
+    <Routes>
+        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/shop' element={<ShopPage/>}></Route>
+    </Routes>
+  </div>
+ )
+
 }
 
 export default App
